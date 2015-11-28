185d8e6c65c51ee1a6d3a751ac9350aa24761d57
Henri Nybom <henri.nybom@gmail.com>
2015-11-28 23:08:18 +0200

    style building system

--- GruntFile.js
+++ GruntFile.js
@@ -31,14 +31,47 @@ module.exports = function(grunt) {
             options: {
                 nospawn: true
             }
+        },
+        concat: {
+            dist: {
+                src: [
+                    'client/sass/**/*.scss',
+                ],
+                dest: 'client/sass/build.scss',
+            }
+        },
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'client/sass',
+                    src: ['build.scss'],
+                    dest: 'public/stylesheets',
+                    ext: '.css'
+                }]
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/stylesheets',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'public/stylesheets',
+                    ext: '.min.css'
+                }]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['browserify:build']);
+    grunt.registerTask('build', ['browserify:build', 'concat', 'sass', 'cssmin']);
 
 };
